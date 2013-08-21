4aec2184c1f3297059e6291a3a5aad80ffc8e485
Justin Karimi <jkarimi@interactions.net>
2013-08-21 11:13:08 -0400

    Update Simulator/Monitor for Analytics
    Update the simulator and monitor processes to handle creating and
    monitoring analytical information about each 'service' in the
    environment and print out the information about each.

--- lib/service_monitor.rb
+++ lib/service_monitor.rb
@@ -3,12 +3,19 @@ require 'parseconfig'
 require 'thread'
 require 'zookeeper'
 
-config  = ParseConfig.new(File.join(File.dirname(__FILE__), "..", "config/settings"))
-ZK_IP   = config['global']['zk_ip']
-ZK_PORT = config['global']['zk_port']
-ZK_PATH = config['global']['zk_path']
+config     = ParseConfig.new(File.join(File.dirname(__FILE__), "..", "config/settings"))
+ZK_IP      = config['global']['zk_ip']
+ZK_PORT    = config['global']['zk_port']
+ZK_PATH    = config['global']['service_subscription_path']
+ZK_SERVICE = config['global']['service_analytics_path']
 
 class ServiceMonitor
+  ##
+  # Initialize the ServiceMonitor instance
+  #
+  # General initialization of parameters and connection to the ZooKeeper
+  # instance where the services are registering/un-registering.
+  #
   def initialize()
     print "Service Monitor Initializing...    "
 
@@ -18,13 +25,18 @@ class ServiceMonitor
     @zk                = Zookeeper::Client.new "#{ZK_IP}:#{ZK_PORT}"
     @watcher           = method(:watcher)
 
-    # ensure that the /services root node is able to be owned by the monitor
+    # ensure that the /services and /service root nodes are able to be owned by the monitor
     begin
       @zk.delete ZK_PATH
     rescue
     end
-
     @zk.create :path => ZK_PATH
+
+    begin
+      @zk.delete ZK_SERVICE
+    rescue
+    end
+    @zk.create :path => ZK_SERVICE
 
     # start the monitoring of services
     @stat = @zk.get_children(:path => ZK_PATH, :watcher => @watcher)[:stat]
@@ -32,29 +44,65 @@ class ServiceMonitor
     print "[ OK ]\n\n".light_green
   end
 
-  def watcher(*args)
+  ##
+  # Keep track of service state
+  #
+  # Watches the ephemeral nodes corresponding to the services in order to keep
+  # track of services registering/un-registering.
+  #
+  def watcher(*opts)
     res              = @zk.get_children :path => ZK_PATH, :watcher => @watcher
     @services, @stat = res[:children], res[:stat]
 
     if @existing_services.size > @services.size # lost a registered service
-      puts "Service(s) with ID #{(@existing_services - @services).join(',').light_red} unregistered\n\n"
+      puts "Service(s) with IP #{(@existing_services - @services).join(', ').light_red} Un-Registered\n\n"
     else                                        # gained a registered service
-      puts "Service(s) with ID #{(@services - @existing_services).join(',').light_green} registered\n\n"
+      puts "Service(s) with IP #{(@services - @existing_services).join(', ').light_green} Registered\n\n"
     end
 
-    puts "SERVICES IDS ONLINE: #{@services.empty? ? 'None'.light_yellow : @services.join(',').light_yellow}\n\n"
+    puts "Service IPs Online: #{@services.empty? ? 'None'.light_yellow : @services.join(', ').light_yellow}\n\n"
 
     @existing_services = @services
   end
 
+  ##
+  # Obtains information about each registered service
+  #
+  # Parses the child nodes of the registered services in order to discover and
+  # print out the state of the subscribed service.
+  #
+  def get_services_state
+    puts "============================================================================" unless @services.empty?
+
+    @services.each do |service_ip|
+      begin
+        print "SERVICE (" + "#{service_ip}".yellow + ") "
+        @zk.get_children(:path => "#{ZK_SERVICE}/#{service_ip}")[:children].each do |child_node|
+          print "| #{child_node.gsub('_', ' ').capitalize} (" + @zk.get(:path => "#{ZK_SERVICE}/#{service_ip}/#{child_node}")[:data].to_s.green + ") "
+        end
+        print "\n"
+      rescue
+      end
+    end
+
+    puts "============================================================================\n\n" unless @services.empty?
+  end
+
+  ##
+  # Main method for the ServiceManager class
+  #
   def run
     @zk.stat(:path => ZK_PATH, :watcher => @watcher) unless @stat.exists
 
+    # obtain state information about each registered service every second
     while true
+      get_services_state
+      sleep 1
     end
   ensure
     @zk.close!
   end
 end
 
+# run an instance of the ServiceMonitor class
 ServiceMonitor.new.run
--- lib/simulator.rb
+++ lib/simulator.rb
@@ -6,22 +6,51 @@ require 'zookeeper'
 config       = ParseConfig.new(File.join(File.dirname(__FILE__), "..", "config/settings"))
 ZK_IP        = config['global']['zk_ip']
 ZK_PORT      = config['global']['zk_port']
-ZK_PATH      = config['global']['zk_path']
-NUM_SERVICES = config['simulator']['num_services']
+ZK_PATH      = config['global']['service_subscription_path']
+ZK_SERVICE   = config['global']['service_analytics_path']
+NUM_SERVICES = config['simulator']['num_services'].to_i
+MAX_CAPACITY = config['simulator']['max_capacity'].to_i
 
 class Simulator
+  ##
+  # Initialize the Simulator instance
+  #
+  # General initialization of parameters and connection to the ZooKeeper
+  # instance where the services will register/un-register.
+  #
   def initialize(service_id)
     @service_id = service_id
     @zk         = Zookeeper::Client.new "#{ZK_IP}:#{ZK_PORT}"
 
-    puts "Simulator Thread Initialized for Service ID: #{@service_id}"
+    puts "Simulator Thread Initialized for Service with IP: #{@service_id}"
   end
 
+  ##
+  # Main method for the Simulator class
+  #
   def run
     # generate a presence notification (ephemeral mode)
     @zk.create :path => "#{ZK_PATH}/#{@service_id}", :ephemeral => true
 
-    sleep rand(10)
+    # generate analytical data
+    begin
+      @zk.delete :path => "#{ZK_SERVICE}/#{@service_id}"
+
+      @zk.create :path => "#{ZK_SERVICE}/#{@service_id}"
+      @zk.create :path => "#{ZK_SERVICE}/#{@service_id}/processing"
+      @zk.create :path => "#{ZK_SERVICE}/#{@service_id}/capacity_remaining"
+
+      # modify the analytical data randomly
+      (1 + rand(10)).times do |counter|
+        new_processing = 1 + rand(9999)
+
+        @zk.set :path => "#{ZK_SERVICE}/#{@service_id}/processing",         :data => new_processing.to_s
+        @zk.set :path => "#{ZK_SERVICE}/#{@service_id}/capacity_remaining", :data => (MAX_CAPACITY - new_processing).to_s
+
+        sleep rand(5)
+      end
+    rescue
+    end
   ensure
     @zk.close!
   end
@@ -29,8 +58,8 @@ end
 
 # run the simulator
 threads = []
-NUM_SERVICES.to_i.times do |counter|
-  threads << Thread.new { Simulator.new(counter).run }
+NUM_SERVICES.times do |counter|
+  threads << Thread.new { Simulator.new("10.0.24.#{counter + 2}").run }
   sleep rand(3)
 end
 
